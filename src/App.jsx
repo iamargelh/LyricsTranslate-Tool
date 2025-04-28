643f86cc2bdfdb178009bf9fdf8f46ff2675107e
iamargelh <l20330537@hermosillo.tecnm.mx>
2025-04-28 05:55:13 -0700

    - added mui dependencies about #1: - update to  lyrics fetching logic to handle synced and plain lyrics - map fetched lyrics -  WIP lyric processing module

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,74 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, use } from 'react'
 import './App.css'
 import { getLyrics } from './services/lrclib.js'
 import { getResultFromIndex } from './logic/lrclib.js'
+import { Button, Input } from '@mui/material'
 
 function App() {
-  const [artistName,setArtistName] = useState()
-  const [trackName,setTraclName] = useState()
-  const [lyricsInfo,setlyricsInfo] = useState()
-  const query = "EDEN - take care"
-
-  const setNewInfo = useCallback((json)=>{
-    const {artist_name, track_name, lyrics} = getResultFromIndex(json,0)
-    console.log({artist_name,track_name,lyrics})
-    setArtistName(artist_name)
-    setTraclName(track_name)
-    setlyricsInfo(JSON.stringify(lyrics))
+  const [search, setSearch] = useState('')
+  const [query,setQuery] = useState('eden take care')
+  const [lyricsInfo,setLyricsInfo] = useState() // MAP
+  const [fullTrackName,setFullTrackName] = useState({
+    artistName: null,
+    trackName: null
   })
 
-  const updateLyrics = useCallback(
+  // no sync: tristam different
+  // sync: eden take care
+
+  const resetApp = ()=>{
+    setFullTrackName({artistName:null,trackName:null})
+    setLyricsInfo(null)
+  }
+
+  const setFromFetch = (json)=>{
+    const {artist_name, track_name, lyrics} = getResultFromIndex(json,0)
+    setFullTrackName({artistName:artist_name,trackName:track_name})
+    setLyricsInfo(lyrics)
+  }
+
+  const fetchLyrics = useCallback(
     (query)=>{
       getLyrics(query)
-      .then(res => setNewInfo(res))
-    }
+      .then(res => setFromFetch(res))
+    },[]
   )
 
   useEffect(
     ()=>{
-      updateLyrics(query)
-    },[]
+      fetchLyrics(query)
+    },[query]
   )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setQuery(search)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    setSearch(newSearch)
+  }
 
   return (
     <>
       <h1>LyricsTranslate Tool</h1>
-      <h2>{(artistName&&trackName) ? `${artistName} - ${trackName}` : "Cargando Titulo..."}</h2>
-      <p>{lyricsInfo ? lyricsInfo : "Cargando Lyrics..."}</p>
+      <form onSubmit={handleSubmit}>
+        <Input onChange={handleChange} value={search} placeholder='eden take care'></Input>
+        <Button type='submit' >Buscar</Button>
+      </form>
+      <h2>
+        {(fullTrackName.artistName && fullTrackName.trackName)
+          ? `${fullTrackName.artistName} - ${fullTrackName.trackName}`
+          : "Cargando Titulo..."}
+      </h2>
+      {
+        (lyricsInfo)
+        ? [...lyricsInfo].map(([key, content])=>{ // Tenga o no lyrics sync, este será un mapa
+            return <p key={key}> {`${content.lyric}`} </p>
+          })
+        : <p>{"Cargando Lyrics..."}</p>
+      }
     </>
   )
 }
--- src/logic/lrclib.js
+++ src/logic/lrclib.js
@@ -3,7 +3,72 @@ export function getResultFromIndex(json,index){
 
     const artist_name = item.artistName
     const track_name = item.trackName
-    const lyrics = item.syncedLyrics ?? item.plainLyrics
+    const lyrics = ()=>{
+        if (item.syncedLyrics) {
+            return processLyricsMap(item.syncedLyrics)
+        } else {
+            return processLyricsMap(item.plainLyrics)
+        }
+    }
 
     return {artist_name,track_name,lyrics}
+}
+
+const HEX_TIMECODES = [
+    /\[(\d{2}:\d{2}\.\d{2,3})\]/,
+    /\[(\d{2}:\d{2}:\d{2}\.\d{2,3})\]/
+];
+
+export function processLyricsMap(string){
+    const lines = string.split("\n")
+    const lrcMap = new Map()
+
+    let timeStampFound = false
+    let noResultCount = 0
+
+    let loopNumber = 1
+    let hex_len = HEX_TIMECODES.length
+    let count = 0
+
+    const saveLineItem = ({line, code, timecode})=>{
+        const lyric = line.replace(code,'').trim()
+        lrcMap.set(`lrc_${String(count).padStart(2,"0")}`,
+            {
+                time_start:timecode,
+                time_end:timecode,
+                lyric:lyric,
+                translation:"",
+                comment:""
+            })
+        count++
+    }
+
+    for(const code of HEX_TIMECODES){
+        noResultCount = 0
+        if (timeStampFound) break
+        for(const line of lines){
+            const match = line.match(code)
+
+            if (!match) {
+                if (noResultCount>=2 && hex_len!==loopNumber){
+                    console.log(`BREAK`)
+                    break
+                }
+                if (hex_len===loopNumber) {
+                    saveLineItem({line, code, timecode:"00:00.00"})
+                } else{
+                    noResultCount++
+                    console.log(`No result, attempt ${noResultCount}`)
+                }
+            } else {
+                saveLineItem({line, code, timecode:match[1]})
+                timeStampFound=true
+            }
+        }
+        loopNumber++
+    }
+
+    if (lrcMap.size===0) return
+    console.log({lrcMap})
+    return lrcMap
 }
